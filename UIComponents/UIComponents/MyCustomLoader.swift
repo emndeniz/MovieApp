2752d13b6b242234d2c8f9314f106e44ffcb871a
emndeniz <emin@zeka.io>
2021-07-29 17:15:18 +0300

    update fatal error

--- UIComponents/UIComponents/MyCustomLoader.swift
+++ UIComponents/UIComponents/MyCustomLoader.swift
@@ -16,7 +16,7 @@ public class MyCustomLoader: UIView{
     }
     
     public required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     func addIndicator(frame: CGRect){
